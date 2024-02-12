44a3136778d447db7ed088fd36e210dab654fbfd
smit-sk <patelsmit7008@gmail.com>
2024-02-12 09:58:36 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-12.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
